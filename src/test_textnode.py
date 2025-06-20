5e43ef51aa31515673bc9e39815670a335e00c34
Alihan Çelikcan <alihancelikcan@gmail.com>
2025-06-20 10:31:19 +0300

    Add unit tests

---
+++ src/test_textnode.py
@@ -0,0 +1,39 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_different_text(self):
+        node1 = TextNode("Text node", TextType.BOLD)
+        node2 = TextNode("Text different", TextType.BOLD)
+        self.assertNotEqual(node1, node2)
+
+    def test_different_types(self):
+        node1 = TextNode("Text node", TextType.BOLD)
+        node2 = TextNode("Text node", TextType.CODE)
+        self.assertNotEqual(node1, node2)
+
+    def test_same_url(self):
+        node1 = TextNode("Text", TextType.LINK, "test_url")
+        node2 = TextNode("Text", TextType.LINK, "test_url")
+        self.assertEqual(node1, node2)
+
+    def test_different_url(self):
+        node1 = TextNode("Text", TextType.LINK, "test_url1")
+        node2 = TextNode("Text", TextType.LINK, "test_url2")
+        self.assertNotEqual(node1, node2)
+
+    def test_link_without_url(self):
+        node1 = TextNode("Text", TextType.LINK)
+        node2 = TextNode("Text", TextType.LINK, "test_url")
+        self.assertNotEqual(node1, node2)
+
+
+if __name__ == "__main__":
+    unittest.main()
